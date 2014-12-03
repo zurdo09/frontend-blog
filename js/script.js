f4ab0565e97e64b246d1363d4f3644fcb20f3fb4
Sebastian Ibarguen <s@cognits.co>
2014-12-03 10:04:03 -0600

    Changed files to json and implemented basic call to files.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 // Javascript Code.
+$(document).ready(function () {
+  // $.get("_posts/2014-11-09.json", function(data){
+  //
+  //   var posts = JSON.parse(data);
+  //   $(".posts").html(posts['posts'][0]['title']);
+  //
+  // });
+  $(this).load("_posts/2014-11-09.json", function(data){
+    var posts = JSON.parse(data);
+    $(".posts").html(posts['posts'][0]['title']);
+    //
+  });
+});
